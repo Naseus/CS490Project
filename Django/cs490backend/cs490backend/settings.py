70d67e7f9ca81a0be90573aaf3bee5a37b14ce75
Naseus <tdevries4i@gmail.com>
2021-09-16 15:12:10 -0400

    fix: requerments and settings

--- Django/cs490backend/cs490backend/settings.py
+++ Django/cs490backend/cs490backend/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Packages
+    'rest_framework'
+
+    # Our Apps
 ]
 
 MIDDLEWARE = [
@@ -98,6 +103,15 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS' : 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE' : 10,
+    'DEFAULT_AUTHENTICATION_CLASSES' : [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ]
+}
 
 
 # Internationalization
