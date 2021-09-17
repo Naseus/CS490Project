277ea4fcf6bf116c4c7f9a055d0cb6ced5169455
Naseus <tdevries4i@gmail.com>
2021-09-17 18:53:39 -0400

    feat: dj-rest-auth implemented

--- Django/cs490backend/cs490backend/settings.py
+++ Django/cs490backend/cs490backend/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # Packages
-    'rest_framework'
+    'rest_framework',
+    'dj_rest_auth',
+    'rest_framework.authtoken',
 
     # Our Apps
 ]
@@ -107,10 +109,6 @@ AUTH_PASSWORD_VALIDATORS = [
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS' : 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE' : 10,
-    'DEFAULT_AUTHENTICATION_CLASSES' : [
-        'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
-    ]
 }
 
 
