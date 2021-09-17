885e25a3dbd0b6bacebbf1e66a62d49ba6380f97
Naseus <tdevries4i@gmail.com>
2021-09-17 19:34:33 -0400

    feat: registration and backend all-auth boilerplate

--- Django/cs490backend/cs490backend/urls.py
+++ Django/cs490backend/cs490backend/urls.py
@@ -20,4 +20,5 @@ from django.urls.conf import include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('dj_rest_auth.urls')),
+    path('auth/registration', include('dj_rest_auth.registration.urls')),
 ]
